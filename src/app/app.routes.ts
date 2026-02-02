f7b9114563bde3848c2d1bf0db65b56d9fe7cef0
Kennybelll <diegocastrol2017@gmail.com>
2026-02-02 14:49:27 -0600

    Agregue Historial Horas e implemente la navegación entre paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,22 @@
 import { Routes } from '@angular/router';
-import { Vereventos} from './paginas/eventos/vereventos/vereventos';
+import { Vereventos } from './paginas/eventos/vereventos/vereventos';
 import { ReporteEventos } from './paginas/admin/reporte-eventos/reporte-eventos';
 import { ReporteHoras } from './paginas/admin/reporte-horas/reporte-horas';
 import { InicioSesion } from './auth/inicio-sesion/inicio-sesion';
 import { Registro } from './auth/registro/registro';
+import { HistorialHoras } from './paginas/historial-horas/historial-horas';
+import { Navegacion } from './paginas/navegacion/navegacion';
 
 
 export const routes: Routes = [
   { path: '', redirectTo: 'admin/reporte-eventos', pathMatch: 'full' },
-  { path: 'admin/reporte-eventos', component: ReporteEventos },
-  { path: 'admin/reporte-horas', component: ReporteHoras },
-  { path: 'eventos/vereventos', component: Vereventos},
+  {
+    path: '', component: Navegacion,
+    children: [{ path: 'admin/reporte-eventos', component: ReporteEventos },
+    { path: 'admin/reporte-horas', component: ReporteHoras },
+    { path: 'eventos/vereventos', component: Vereventos },
+    { path: 'historial-horas', component: HistorialHoras },]
+  },
   { path: 'inicio-sesion', component: InicioSesion },
   { path: 'registro', component: Registro },
 ];
--- src/app/paginas/historial-horas/historial-horas.ts
+++ src/app/paginas/historial-horas/historial-horas.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './historial-horas.css',
 })
 export class HistorialHoras {
-
+  
 }
