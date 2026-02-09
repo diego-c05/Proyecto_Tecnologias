52d1a84a5b18d557b3b1650445fb3614ffbf08b8
arielbmz <bonillaariel638@gmail.com>
2026-02-09 09:50:34 -0600

    Implementación de autenticación con Firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDu_XtcUzEUI7Hy-cNu78gmD6dh-zp3b24",
+    authDomain: "gestion-horas-de-vinculacion.firebaseapp.com",
+    projectId: "gestion-horas-de-vinculacion",
+    storageBucket: "gestion-horas-de-vinculacion.firebasestorage.app",
+    messagingSenderId: "778904745467",
+    appId: "1:778904745467:web:1871ec0e6af101cb40c75e"
+  }
+};
